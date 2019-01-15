49eb6323420ad92b2dc9a859da783a7342b283fb
alphanu1 <37101891+alphanu1@users.noreply.github.com>
2019-01-15 21:53:00 +0000

    Update video_crt_switch.c

--- gfx/video_crt_switch.c
+++ gfx/video_crt_switch.c
@@ -215,22 +215,7 @@ void crt_rpi_switch(void)
    
     if (fork() == 0) {
 
-		 
-		   set_hdmi_timings();
-   exit(0);
-
-   
-       
-    }
-   sprintf(output1,"tvservice -e \"DMT 87\" > /dev/null");
-   system(output1);
-   sprintf(output2,"fbset -g 1920 240 1920 240 24 > /dev/null");
-   system(output2);
-}
-
-void set_hdmi_timings (void)
-{
-static const char set_hdmi_timing[] = "hdmi_timings 1920 1 106 169 480 240 1 1 3 5 0 0 0 60 0 41458500 1 ";
+		static const char set_hdmi_timing[] = "hdmi_timings 1920 1 106 169 480 240 1 1 3 5 0 0 0 60 0 41458500 1 ";
 VCHI_INSTANCE_T vchi_instance;
 VCHI_CONNECTION_T *vchi_connection = NULL;
 char buffer[1024];
@@ -255,4 +240,15 @@ vc_gencmd (buffer, sizeof (buffer), set_hdmi_timing);
     //close the vchi connection
    vchi_disconnect (vchi_instance);
        // fatal ("VCHI disconnect failed");
+   exit(0);
+
+   
+       
+    }
+   sprintf(output1,"tvservice -e \"DMT 87\" > /dev/null");
+   system(output1);
+   sprintf(output2,"fbset -g 1920 240 1920 240 24 > /dev/null");
+   system(output2);
 }
+
+
