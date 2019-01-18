e0a00615fe04166854d7c7584073f5649580f460
alphanu1 <37101891+alphanu1@users.noreply.github.com>
2019-01-18 22:03:03 +0000

    Add files via upload

--- gfx/video_crt_switch.c
+++ gfx/video_crt_switch.c
@@ -151,7 +151,7 @@ void crt_screen_setup_aspect(unsigned width, unsigned height)
       crt_aspect_ratio_switch(width, height);
       height = 254;
    }
-
+	switch_res_crt(width, height);
  }
 
 
@@ -172,9 +172,7 @@ void crt_switch_res_core(unsigned width, unsigned height, float hz)
       )
 	  {
     crt_screen_setup_aspect(width, height);
-	switch_res_crt(width, height);
-
-	}
+	  }
    ra_tmp_height  = ra_core_height;
    ra_tmp_width   = ra_core_width;
 
