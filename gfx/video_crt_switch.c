83d228974b35a55d6a46ed310b58d187e133c511
alphanu1 <37101891+alphanu1@users.noreply.github.com>
2019-01-18 21:16:43 +0000

    Add files via upload

--- gfx/video_crt_switch.c
+++ gfx/video_crt_switch.c
@@ -348,7 +348,8 @@ void crt_rpi_switch(int width, int height, int hz)
    
    // if (fork() == 0) {
       sprintf(set_hdmi, "hdmi_timings 1920 1 106 169 480 240 1 1 3 5 0 0 0 60 0 41458500 %d ", 1);
-	  sprintf(set_hdmi_timing, "hdmi_timings %d 1 %d %d %d %d 1 %d %d %d 0 0 0 %d 0 %f 1 ", width, hfp, hsp, hbp, height, vfp, vsp, vbp, hz, pixel_clock); 
+	//  sprintf(set_hdmi_timing, "hdmi_timings %d 1 %d %d %d %d 1 %d %d %d 0 0 0 %d 0 %f 1 ", width, hfp, hsp, hbp, height, vfp, vsp, vbp, hz, pixel_clock); 
+	  	  sprintf(set_hdmi_timing, "hdmi_timings %d 1 %d %d %d %d 1 %d %d %d 0 0 0 %d 0 %f 1 ", 1920, 106, 169, 480, 240, 1, 3, 5, 60,41458500); 
    //HRES, HSYNCPOLARITY, HFRONTPORCH, HSYNCPORCH, HBACKPORCH, VRES, VSYNCPOLARITY, VFRONTPORCH, VSYNCPULSE, VBACKPORCH, 0, 0, 0, HZ, PROG/INTERLACED, DOTCLOCK, 1
 	 //  set_hdmi_timing[] = set_hdmi;
       VCHI_INSTANCE_T vchi_instance;
