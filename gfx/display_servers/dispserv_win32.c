a7b722518b1a3ee836c27c225a346b2f83b95d13
alphanu1 <37101891+alphanu1@users.noreply.github.com>
2019-01-28 16:25:31 +0000

    Update dispserv_win32.c

--- gfx/display_servers/dispserv_win32.c
+++ gfx/display_servers/dispserv_win32.c
@@ -208,7 +208,7 @@ static bool win32_set_window_decorations(void *data, bool on)
 }
 
 static bool win32_display_server_set_resolution(void *data,
-      unsigned width, unsigned height, int int_hz, float hz)
+      unsigned width, unsigned height, int int_hz, float hz, int crt_monitor_index)
 {
    _beginthread(win32_display_server_set_resolution_thread,0 , NULL);
     dispserv_win32_t *serv = (dispserv_win32_t*)data;
