dec4b74843d378ea23d738ba089494b2137faa33
alphanu1 <37101891+alphanu1@users.noreply.github.com>
2019-01-28 15:47:42 +0000

    Update dispserv_x11.c

--- gfx/display_servers/dispserv_x11.c
+++ gfx/display_servers/dispserv_x11.c
@@ -108,7 +108,7 @@ static bool x11_set_window_decorations(void *data, bool on)
 }
 
 static bool x11_set_resolution(void *data,
-      unsigned width, unsigned height, int int_hz, float hz)
+      unsigned width, unsigned height, int int_hz, float hz, int monitor_index)
 {
   video_monitor_set_refresh_rate(hz);	 
 if (fork() == 0)
